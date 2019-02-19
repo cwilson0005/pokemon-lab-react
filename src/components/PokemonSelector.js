158e4f08bdedc3a7e4f1df0c4e4e5bea6426f73a
Conor <cwilson0005@hotmail.co.uk>
2019-02-19 13:42:13 +0000

    working on getting map method to work

--- src/components/PokemonSelector.js
+++ src/components/PokemonSelector.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const PokemonSelector = (props) => {
-
+  console.log("props:", props);
   const options = props.allPokemon.map((pokemon, index) => {
-    return <option value={name}>{Pokemon.name}</option>
+    return <option value={index} key={index}>{pokemon.name}</option>
   })
 
   function handleChange(event){
@@ -13,7 +13,7 @@ const PokemonSelector = (props) => {
   return (
     <select id="Pokemon-selector" defaultValue="default" onChange={handleChange}>
       <option disabled value="default">Choose a Pokemon...</option>
-      {options}
+      {options.results}
     </select>
   )
 }
--- src/containers/PokemonBox.js
+++ src/containers/PokemonBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import PokemonSelector from '../components/PokemonSelector';
 import PokemonDetail from '../components/PokemonDetail';
 
