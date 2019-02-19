ff43819f080f51771fbe71c6675e0b29382edffc
Conor <cwilson0005@hotmail.co.uk>
2019-02-19 14:05:06 +0000

    working on getting options to be recognised

--- src/components/PokemonSelector.js
+++ src/components/PokemonSelector.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const PokemonSelector = (props) => {
   console.log("props:", props);
-  const options = props.allPokemon.map((pokemon, index) => {
-    return <option value={index} key={index}>{pokemon.name}</option>
-  })
+  if (props.allPokemon.results){
+    const options = props.allPokemon.results.map((pokemon, index) => {
+      return <option value={index} key={index}>{pokemon.name}</option>
+    })
+    console.log(options);
+  }
 
   function handleChange(event){
     props.handlePokemonSelected(event.target.value);
@@ -13,7 +16,7 @@ const PokemonSelector = (props) => {
   return (
     <select id="Pokemon-selector" defaultValue="default" onChange={handleChange}>
       <option disabled value="default">Choose a Pokemon...</option>
-      {options.results}
+      {options.props.children}
     </select>
   )
 }
